74dbd784f4a6e28288c64821450a27e635b66a50
theCarrierOfGod <olajesujuwon@gmail.com>
2023-10-10 22:32:46 +0100

    Init

--- src/pages/ads/Refer.js
+++ src/pages/ads/Refer.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 export const Refer = () => {
     return (
         <>
-            <div className='col-md-12 strech-card grid-margin'>
-                <div className='notification is-info is-light p-1' style={{ fontFamily: 'monospace', fontSize: '8px' }}>
+            <div className='col-md-12 grid-margin stretch-card'>
+                <div className='notification is-info is-light' style={{ fontFamily: 'monospace', fontSize: '8px' }}>
                     <p>Earn daily 5% ROI on Thelikey Invest. Create a new account to unlock #100 bonus. <Link to={'https://mlm.thelikey.com/about.php'}>Click here to Learn More</Link></p>
                 </div>
             </div>
