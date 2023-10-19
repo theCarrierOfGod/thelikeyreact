b6760121124b69d39cc6089c1d5d0b2abfcd0eed
theCarrierOfGod <olajesujuwon@gmail.com>
2023-10-19 20:49:14 +0100

    with

--- src/user/wallet/Withdraw.js
+++ src/user/wallet/Withdraw.js
@@ -388,7 +388,7 @@ const Withdraw = () => {
                                     </div>
                                 </div>
 
-                                <div className="col-md-12 grid-margin stretch-card">
+                                <div className="col-md-6 grid-margin stretch-card">
                                     <div className="card">
                                         <div className="card-body">
                                             <p className="card-description">
